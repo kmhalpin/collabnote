adb2983607610dc8ac2cb7af2f394d69208f76c3
kmhalpin <53615235+kmhalpin@users.noreply.github.com>
2023-01-06 11:35:23 +0700

    inheritance

--- client/src/jmh/java/com/collabnote/client/ClientBenchmark.java
+++ client/src/jmh/java/com/collabnote/client/ClientBenchmark.java
@@ -1,42 +1,115 @@
 package com.collabnote.client;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.text.BadLocationException;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 
-import com.collabnote.crdt.CRDTItem;
+class InputData {
+    public int index;
+    public String character;
+
+    public InputData(int index, String character) {
+        this.index = index;
+        this.character = character;
+    }
+}
 
 public class ClientBenchmark {
+    static final int N = 100;
+    static final int warmUpN = 5;
 
     @State(Scope.Benchmark)
-    public static class MyState {
-        private App app = new App(false);
-        public int i = 0;
+    public static class DefaultStateImpl extends DefaultState {
+        @Override
+        public void doSetup() {
+            String[] dataArray = this.rawData.split("");
+            for (int i = 0; i < N + warmUpN; i++) {
+                this.data.add(new InputData(i, dataArray[i]));
+            }
+        }
     }
 
     @Benchmark
     @BenchmarkMode(Mode.SingleShotTime)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Measurement(batchSize = 1, iterations = 100)
-    @Warmup(iterations = 0)
-    public List<CRDTItem> insertOperations(MyState state) throws BadLocationException {
-        state.app.insertCRDT(state.i, "a");
+    @Measurement(batchSize = 1, iterations = N)
+    @Warmup(iterations = warmUpN)
+    public void appendCharacters(DefaultStateImpl state) throws BadLocationException {
+        state.app.insertCRDT(state.data.get(state.i).index, state.data.get(state.i).character);
         state.i += 1;
-
-        return state.app.getCRDT().returnCopy();
     }
+
+    @Benchmark
+    @BenchmarkMode(Mode.SingleShotTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Measurement(batchSize = 1, iterations = N)
+    @Warmup(iterations = warmUpN)
+    public void prependCharacters(DefaultStateImpl state) throws BadLocationException {
+        state.app.insertCRDT(0, state.data.get(state.i).character);
+        state.i += 1;
+    }
+
+    @State(Scope.Benchmark)
+    public static class RandomDataState extends DefaultState {
+        @Override
+        public void doSetup() {
+            String[] dataArray = this.rawData.split("");
+            for (int i = 0; i < N + warmUpN; i++) {
+                this.data.add(new InputData(ThreadLocalRandom.current().nextInt(0, i + 1), dataArray[i]));
+            }
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.SingleShotTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Measurement(batchSize = 1, iterations = N)
+    @Warmup(iterations = warmUpN)
+    public void randomPositionCharacters(RandomDataState state) throws BadLocationException {
+        state.app.insertCRDT(state.data.get(state.i).index, state.data.get(state.i).character);
+        state.i += 1;
+    }
+
+    @State(Scope.Benchmark)
+    public static abstract class DefaultState {
+        App app;
+        List<InputData> data;
+        String rawData;
+        int i;
+
+        @TearDown
+        public void doTearDown() {
+            System.out.println("result:\n" + this.app.getCRDT().toString());
+        }
+
+        public abstract void doSetup();
+
+        @Setup
+        public void setup() {
+            this.app = new App(false);
+            this.i = 0;
+            this.data = new ArrayList<>();
+
+            this.rawData = RandomStringUtils.randomAscii(N + warmUpN);
+            System.out.println("data:\n" + rawData);
+            doSetup();
+        }
+    }
+
 }
